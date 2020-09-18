7bec4a0ab12192900ca153611eb30579c05e9afc
Will Brand <will.brand@gmail.com>
2020-09-18 08:47:53 -0700

    Movies class

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,22 @@
 import sys
 
+class Movie():
+    def __init__(self, line):
+        sp = line.split(',')
+        if len(sp) != 4: 
+            return False # todo: better error to throw
+        
+        self.title = sp[0]
+        self.year = int(sp[1])
+        self.rating = sp[2]
+        
+        (hours, minutes) = sp[3].split(':')
+        
+        self.duration = int(hours) * 60 + int(minutes)
+        
+    def __repr__(self):
+        return self.title + ' - Rated ' + self.rating + ', ' + str(self.duration // 60) + ':' + ("%02d" % (self.duration % 60)) 
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python scheduler.py <inputFile>")
@@ -9,8 +26,9 @@ def main():
 
     movies = []
     with open(filename, 'r') as f:
-        movies = f.readlines()[1:]
-        
+        for line in f.readlines()[1:]:
+            movies.append(Movie(line))
+            
     print(movies)
     
 if __name__ == '__main__':
