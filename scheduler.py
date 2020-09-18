dbbe6900164d58af95a8587cd333a7dbeca2da4f
Will Brand <will.brand@gmail.com>
2020-09-18 08:52:34 -0700

    Basic test for csv parsing

--- scheduler.py
+++ scheduler.py
@@ -2,13 +2,13 @@ import sys
 
 class Movie():
     def __init__(self, line):
-        sp = line.split(',')
+        sp = line.strip().split(',')
         if len(sp) != 4: 
             return False # todo: better error to throw
         
         self.title = sp[0]
         self.year = int(sp[1])
-        self.rating = sp[2]
+        self.rating = sp[2].strip()
         
         (hours, minutes) = sp[3].split(':')
         
@@ -17,6 +17,14 @@ class Movie():
     def __repr__(self):
         return self.title + ' - Rated ' + self.rating + ', ' + str(self.duration // 60) + ':' + ("%02d" % (self.duration % 60)) 
 
+def parse(filename): 
+    movies = []
+    with open(filename, 'r') as f:
+        for line in f.readlines()[1:]:
+            movies.append(Movie(line))
+           
+    return movies
+    
 def main():
     if len(sys.argv) < 2:
         print("Usage: python scheduler.py <inputFile>")
@@ -24,12 +32,7 @@ def main():
        
     filename = sys.argv[1]
 
-    movies = []
-    with open(filename, 'r') as f:
-        for line in f.readlines()[1:]:
-            movies.append(Movie(line))
-            
-    print(movies)
+    movies = parse(filename)
     
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 import unittest
+import scheduler
 
+class TestScheduler(unittest.TestCase):
+    def test_parsing(self):
+        parsed = scheduler.parse('inputs/good.txt')
+        self.assertEqual(parsed[0].__repr__(), "There's Something About Mary - Rated R, 2:14")
+        
+if __name__ == "__main__":
+    unittest.main()
